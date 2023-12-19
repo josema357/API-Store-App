bef7bf513a2d52e6ad6a37d5130c3ee5b8e1c4a9
Jose Manuel <j.ochoa317@gmail.com>
2023-12-19 09:48:47 -0500

    [UPDATE] dotenv and CORS

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express');
 const routerAPI = require('./app/routerAPI')
 const {logErrors, errorHandler}=require('./Middlewares/errorHandler');
 const {boomErrorHandler}=require('./Middlewares/boomErrorHandler');
+require('dotenv').config()
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+//CORS configuration
+const whiteListString=process.env.WHITELIST || 'http://localhost:4000';
+const whiteList = whiteListString.split(",");
+const options={
+  origin:(origin, callback)=>{
+    if(whiteList.includes(origin) || !origin){
+      callback(null, true);
+    }else{
+      callback(new Error('Not Allowed by CORS'), false)
+    }
+  }
+}
+app.use(cors(options));
 
 app.use(express.json());
 
@@ -14,6 +30,7 @@ app.get('/', (req, res) => {
 
 routerAPI(app);
 
+//Middlewares
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
