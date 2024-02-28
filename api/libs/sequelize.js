fb0292891282918fb4071c489de3fcf90e09aa27
Jose Manuel <j.ochoa317@gmail.com>
2024-02-28 11:04:15 -0500

    Change to Postgres

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 
 const USER=encodeURIComponent(process.env.DB_USER);
 const PASSWORD=encodeURIComponent(process.env.DB_PASSWORD);
-const URI = `mysql://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+//const URI = `mysql://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const URI = `postgres://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const sequelize = new Sequelize(URI,{
-    dialect:'mysql'
+    dialect:'postgres'
 });
 
 setupModels(sequelize);
