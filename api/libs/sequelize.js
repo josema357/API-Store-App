c8db1cd70dc5918399c14b4ec4428ee4a5acc0b5
Jose Manuel <j.ochoa317@gmail.com>
2024-03-04 11:04:48 -0500

    bugs fixed

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -2,9 +2,8 @@ const {Sequelize} = require('sequelize');
 const setupModels = require('../app/models');
 require('dotenv').config();
 
-//const USER=encodeURIComponent(process.env.DB_USER);
-//const URI = `postgres://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-const URI = `postgres://root:${process.env.PG_DB_PASSWORD}:${process.env.PG_DB_PORT}/my_store_xjnl`
+//const URI = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const URI = `postgres://root:${process.env.PG_DB_PASSWORD}@${process.env.PG_DB_HOST}/my_store_xjnl`
 const sequelize = new Sequelize(URI,{
     dialect:'postgres',
     ssl: true
