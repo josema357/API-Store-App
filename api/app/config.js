4b68af0efa5fa9b4098dfb42a936b81e12f07041
Jose Manuel <j.ochoa317@gmail.com>
2024-03-05 11:27:39 -0500

    bugs remote access mysql fixed

--- api/app/config.js
+++ api/app/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const URI = `mysql://root:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 //const URI = `postgres://root:${process.env.PG_DB_PASSWORD}@${process.env.PG_DB_HOST}/my_store_xjnl`
 
 module.exports = {
--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 const setupModels = require('../app/models');
 require('dotenv').config();
 
-const URI = `mysql://root:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 //const URI = `postgres://root:${process.env.PG_DB_PASSWORD}@${process.env.PG_DB_HOST}/my_store_xjnl`
 const sequelize = new Sequelize(URI,{
     dialect:'mysql'
