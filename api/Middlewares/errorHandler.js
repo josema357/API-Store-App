970e00f02d1e0c4eaac3f19dcb0fb6a643da6adc
Jose Manuel <j.ochoa317@gmail.com>
2024-03-05 10:53:14 -0500

    modified middleware

--- api/Middlewares/errorHandler.js
+++ api/Middlewares/errorHandler.js
@@ -2,8 +2,8 @@ function logErrors(err, req, res, next){
     next(err);
 }
 
-function errorHandler(err, req, res){
-    res.json({
+function errorHandler(err, req, res, next){
+    res.status(500).json({
         message: err.message,
         stack: err.stack
     });
